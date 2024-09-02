687bc238343a86d9ae85182e265c58a86e2f5235
Charles Schlue <schluecharlesr@johndeere.com>
2024-09-02 04:08:55 -0500

    move api to config

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiUrl: 'https://services.arcgis.com/8lRhdTsQyJpO52F1/arcgis/rest/services/Traffic_Cameras_View/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=true&f=pjson',
+    maxSimultaneousVideos: 4,
+    lazyLoadOptions: {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    }
+}
